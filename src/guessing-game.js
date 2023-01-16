7d90d03bb024b3ba0ea75de899400b4a655aab8c
Yauheniya Mastavaya <jk@db.by>
2023-01-16 19:43:05 +0300

    feat: implement guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.answer = Math.round((this.min + this.max) / 2);
+        return this.answer;
     }
 
     lower() {
-
+        this.max = this.answer;
     }
 
     greater() {
-
+        this.min = this.answer;
     }
 }
 
